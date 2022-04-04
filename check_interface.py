a220c7896f5287d9ea6bfe9d1470e1eca134dd8c
hujie331 <hujie.331@gmail.com>
2022-04-03 22:48:37 -0700

    Updated: 2022-04-03 22:48:37

--- check_interface.py
+++ check_interface.py
@@ -1,6 +1,11 @@
 import sjca
 import bp
 import sys,time
+import signal
+
+
+signal.signal(signal.SIGPIPE, signal.SIG_DFL)  # IOError: Broken pipe
+signal.signal(signal.SIGINT, signal.SIG_DFL)  # KeyboardInterrupt: Ctrl-C
 
 
 def invalid_choice():
